16c4be5711870b617f24eb43772867cafb98d489
kijing <52029855+kijing@users.noreply.github.com>
2019-08-23 17:05:45 +0800

    Update read_write.py

--- read_write.py
+++ read_write.py
@@ -12,11 +12,12 @@ def read():
 def write():
     ff=open('2.txt','a+') #  新建文件名  a+表示读写
     fff=open('1.txt')
-    line = fff.read()   # read 读出来的是整个文件字符串 readline 读出来的是行 readlines读出来的是整个文件的列表 按照行的读取方式
+    line = fff.read()   # read 读出来的是整个文件字符串 readline 读出来的是行 readlines读出来的是整个文件的列表 按照行的
+读取方式
     ff.write(line)
     time.sleep(1)
 
-if __name=__==__'main'__:  #if 在自身运行改文件的时候会生效，但是在import 该py文件时，不会运行以下程序
+if __name__=='__main__':  # if 在自身运行改文件的时候会生效，但是在import 该py文件时，不会运行以下程序
   t1=threading.Thread(target=read)
   t2=threading.Thread(target=write)
   t1.start()
